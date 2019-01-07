0838313e183c49cc531120d0e0660384958cb223
Marton Ady <marton.ady@cern.ch>
2019-01-07 14:08:18 +0100

    New folder structure, 2.7.3, including Fedora build

---
+++ build_mac/clean_mac.sh
@@ -0,0 +1,1 @@
+rm ../intermediary/mac/*.o
---
+++ build_mac/compile_mac_temp.sh
@@ -0,0 +1,3 @@
+cd ../intermediary/mac
+g++-8 -c -std=c++17 -Wno-write-strings -DMOLFLOW -O3 -I/usr/local/include -I../../source/molflow_code -I../../source/shared_code -I../../include -I../../include/SDL ../../source/molflow_code/SubProcessFacet.cpp
+cd ../../build_mac
