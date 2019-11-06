2c890d01e04263c6b9ef2ca7b6ba93cca707e009
Pascal Baehr <pascal.baehr@cern.ch>
2019-11-06 10:14:58 +0100

    Changes to CMake files; Some changes for better debugging support; Added GUI test with gtest on Linux

---
+++ lib_external/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pascal/cernbox/Projects/googletest
+# Build directory: /home/pascal/cernbox/Projects/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ lib_external/googletest/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pascal/cernbox/Projects/googletest/googlemock/src/gmock-all.cc" "/home/pascal/cernbox/Projects/googletest/googlemock/CMakeFiles/gmock.dir/src/gmock-all.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "googlemock/include"
+  "googlemock"
+  "googletest/include"
+  "googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pascal/cernbox/Projects/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
