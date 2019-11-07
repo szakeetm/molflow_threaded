a03ab44d949cbd45785547623f5caaecbaf14585
Pascal Baehr <pascal.baehr@cern.ch>
2019-11-07 07:55:54 +0100

    Modified .gitignore and cleaned up repository

--- bin/linux_fedora/launch_molflow.sh
+++
@@ -1,3 +0,0 @@
-chmod +x molflow 7za compress
-LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH ./molflow
-
--- lib_external/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pascal/cernbox/Projects/googletest/googletest
-# Build directory: /home/pascal/cernbox/Projects/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
