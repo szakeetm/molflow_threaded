1f266b99e652e6e8fa59c6877079edc90cc7362f
Marton Ady <marton.ady@cern.ch>
2019-11-07 10:22:44 +0100

    Changed Mac build/link scripts to create build directories not in the online repo

--- build_mac/link_mac.sh
+++ build_mac/link_mac.sh
@@ -1,3 +1,4 @@
+mkdir -p ../bin/mac
 cd ../bin/mac
 g++-9 -o molflow -L/usr/local/lib ../../precomp_obj/mac/*.o ../../intermediary/mac/*.o -framework OpenGL -lSDL2 -lpng -lgsl -lcurl -lstdc++ -lstdc++fs -framework AppKit
 cd ../../build_mac
